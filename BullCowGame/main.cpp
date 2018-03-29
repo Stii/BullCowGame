a5541494f96133a1b0ecb08ec23ef9f9bfe4d00e
Stii <stii@takealot.com>
2018-03-29 17:20:01 +0200

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Stii on 2018/03/29.
+//  Copyright © 2018 Stii. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
